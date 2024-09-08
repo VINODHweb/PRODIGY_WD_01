329c98d0463e85878380bd08afbdd37f19d50974
VINODH AM <vinodham155@gmail.com>
2024-09-08 20:32:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var toggle = document.querySelector (".toggle")
+
+function showtoggle()
+{
+    toggle.style.display="block"
+}
+function closetoggle()
+{
+    toggle.style.display="none"
+}
+
+var login=document.getElementById("login")
+function update()
+{
+    login.style.backgroundColor="orangered"
+}
